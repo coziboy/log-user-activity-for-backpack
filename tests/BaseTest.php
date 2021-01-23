50e9a1714edcc763d4932812013d67937f0ee4ab
Andreas Asatera <ryusuke005@gmail.com>
2021-01-23 10:02:21 +0700

    Base Test

---
+++ tests/BaseTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Coziboy\LogUserActivityForBackpack\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class BaseTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
